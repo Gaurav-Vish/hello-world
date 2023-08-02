ec8fd9050ecd14d09e844e1d1b12b4e55f3c43cf
Gaurav <gvishwakarma768@gmail.com>
2023-08-02 13:46:40 -0400

    counter and callback function value

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message'
+import Counter from './components/Counter';
 import { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message/>
+        <Counter></Counter>
+        {/* <Message/> */}
         {/* <Greet name="Bruce" heroName="Batman">
           <p>This is a child prop</p>
         </Greet>
