ffc624b1c1301a3e5030161dd5a87a8fdbe99508
Gaurav <gvishwakarma768@gmail.com>
2023-08-02 14:06:08 -0400

    destructuring props and state

--- src/App.js
+++ src/App.js
@@ -11,18 +11,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter></Counter>
+        {/* <Counter></Counter> */}
         {/* <Message/> */}
         {/* <Greet name="Bruce" heroName="Batman">
           <p>This is a child prop</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
           <button>Action</button>
-        </Greet>
-        <Greet name="Diana" heroName="Wonder Woman">
         </Greet> */}
-        {/* <Welcome name="Bruce" heroName="Batman"/>
-        <Welcome name="Clark" heroName="Superman"/>
+        <Greet name="Diana" heroName="Wonder Woman">
+        </Greet>
+        <Welcome name="Bruce" heroName="Batman"/>
+        {/* <Welcome name="Clark" heroName="Superman"/>
         <Welcome name="Diana" heroName="Wonder Woman"/> */}
         {/* <Hello/> */}
       </div>
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,11 +6,13 @@ import React from "react";
 
 // export const Greet = () => <h1>Hello Gaurav</h1>
 const Greet = props => {
-    console.log(props)
+    const {name, heroName} = props
     return (
         <div>
-            <h1>Hello {props.name} a.k.a. {props.heroName} </h1>
-            {props.children}
+            <h1>
+                Hello {name} a.k.a. {heroName}
+            </h1>
+            {/* {props.children} */}
         </div>
     )
 }
